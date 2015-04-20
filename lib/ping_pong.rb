ebb997664381a03a2bdeefbf74b003e9c5b9a19a
Ian MacDonald & David Abel <student@epicodus.com>
2015-04-20 10:30:19 -0700

    method should list numbers up to number given

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -1,6 +1,9 @@
 class Fixnum
   define_method(:ping_pong) do
     ping_pong_array = []
-    ping_pong_array
+    numbers = (1..self)
+    nuber.each() do |number|
+      ping_pong_array.push(number)  
+    end
   end
 end
