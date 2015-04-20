17ca730e8a582528035c90a44d4481c4f755dcef
Ian MacDonald & David Abel <student@epicodus.com>
2015-04-20 10:40:45 -0700

    ping_pong returns array with ping instead of multiples of 3

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -3,7 +3,11 @@ class Fixnum
     ping_pong_array = []
     numbers = (1..self)
     numbers.each() do |number|
-      ping_pong_array.push(number)
+      if number.%(3).eql?(0)
+        ping_pong_array.push("ping")
+      else
+        ping_pong_array.push(number)
+      end
     end
     ping_pong_array
   end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -5,4 +5,7 @@ describe('Fixnum#ping_pong') do
   it("when number is entered, returns an array from 1 to number entered") do
     expect(1.ping_pong()).to(eq([1]))
   end
+  it("when number is entered, returns an array from 1 to number entered") do
+    expect(4.ping_pong()).to(eq([1,2,"ping",4]))
+  end
 end
