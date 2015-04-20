a311e846197d27e9f9fd4418fca1a7dc05667526
Ian MacDonald & David Abel <student@epicodus.com>
2015-04-20 10:34:14 -0700

    method returns array of numbers up to number given

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -2,8 +2,9 @@ class Fixnum
   define_method(:ping_pong) do
     ping_pong_array = []
     numbers = (1..self)
-    nuber.each() do |number|
-      ping_pong_array.push(number)  
+    numbers.each() do |number|
+      ping_pong_array.push(number)
     end
+    ping_pong_array
   end
 end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -2,9 +2,6 @@ require('rspec')
 require('ping_pong')
 
 describe('Fixnum#ping_pong') do
-  it("when number is entered, returns an array") do
-    expect(1.ping_pong()).to(eq([]))
-  end
   it("when number is entered, returns an array from 1 to number entered") do
     expect(1.ping_pong()).to(eq([1]))
   end
