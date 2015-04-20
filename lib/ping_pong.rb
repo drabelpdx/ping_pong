2ae4b8a31aa76791f3bf7012e3c16d1d036ca32e
Ian MacDonald & David Abel <student@epicodus.com>
2015-04-20 10:52:30 -0700

    ping_pong method complete

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -3,8 +3,12 @@ class Fixnum
     ping_pong_array = []
     numbers = (1..self)
     numbers.each() do |number|
-      if number.%(3).eql?(0)
+      if number.%(3).eql?(0) and number.%(5).eql?(0)
+        ping_pong_array.push("ping-pong")
+      elsif number.%(3).eql?(0)
         ping_pong_array.push("ping")
+      elsif number.%(5).eql?(0)
+        ping_pong_array.push("pong")
       else
         ping_pong_array.push(number)
       end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -5,7 +5,13 @@ describe('Fixnum#ping_pong') do
   it("when number is entered, returns an array from 1 to number entered") do
     expect(1.ping_pong()).to(eq([1]))
   end
-  it("when number is entered, returns an array from 1 to number entered") do
+  it("when number is entered, returns an array from 1 to number entered, with multiples of 3 being replaced with 'ping'") do
     expect(4.ping_pong()).to(eq([1,2,"ping",4]))
+  end
+  it("when number is entered, returns an array from 1 to number entered, with multiples of 3 being replaced with 'ping', and multiples of 5 replaced with 'pong'") do
+    expect(5.ping_pong()).to(eq([1,2,"ping",4,"pong"]))
+  end
+  it("when number is entered, returns an array from 1 to number entered, with multiples of 3 being replaced with 'ping', multiples of 5 replaced with 'pong', and multiples of 3 & 5 are replaced with 'ping-pong'") do
+    expect(15.ping_pong()).to(eq([1,2,"ping",4,"pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "ping-pong"]))
   end
 end
